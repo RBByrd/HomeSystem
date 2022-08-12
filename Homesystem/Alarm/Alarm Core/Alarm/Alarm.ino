6e79f7a1bde1d96e9f400be294b80964413f8ca9
byrd798 <byrd798@yahoo.com>
2022-08-11 20:57:39 -0500

    added real time measuring to the application, tested with LCD backlight

--- Homesystem/Alarm/Alarm Core/Alarm/Alarm.ino
+++ Homesystem/Alarm/Alarm Core/Alarm/Alarm.ino
@@ -9,7 +9,9 @@
    * 
    */
   
-  
+  #include <Wire.h> 
+  //using https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library.git
+  #include <LiquidCrystal_I2C.h>
   #include <EEPROM.h>
 
   //Global Variables. Variables that will be used for the initial setup and during operation
@@ -30,31 +32,134 @@
   int EEAddArmed = 32;
   int EEArmed;
 
+  //Initialize lcd varialble
+  LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
+  //LCD boot
+  LCDInitialize();
+  
+  
   //tests if the system has been setup before. Does not progress if the system has not been setup, or the memory has become corrupt.
   setupCheck();
 
   //set the zones to the correct HW state
   setZones();
 
-
   zoneRead(zoneStates);
 }
 
+
+//loop variables
+
+//RT (Real Time) variables need to be unsigned long to interact with the millis() function
+
+//RTinteration is the latest time that the system has seen interaction.
+unsigned long RTinteraction = millis();
+
+//should be changed from 0 to 1 if there has been interation this cycle
+bool interaction;
+
+int serialData;
+
+
+/*The loop function should be thought of as a while(1) loop. 
+*
+*Keeping this in mind each function used in the loop should be executed in a timely manner that does not hold up the system 
+*
+*/
 void loop() {
-  // put your main code here, to run repeatedly:
- 
-  zoneRead(zoneStates);
+  //Serial.println(LCDbldelay);
+  
+  //initialize loop
+  interaction = 0;
 
-  printZones();
-  Serial.println(zoneStates[0]);
-  delay(1000);
+  //loop inputs
+  //zoneRead(zoneStates, &interaction);
+  //keyPad();
+  serialInput();
+
+
+  //system functions
+  setInteractionTime(interaction, &RTinteraction);
+
+  //loop outputs
+  //printZones();
+  LCDbacklight(RTinteraction);
+  //serialOutput();
 
 
 }
+
+void serialInput(){
+   if (Serial.available() > 0){
+      serialData = Serial.read();
+      Serial.println(serialData);
+      interaction = 1;
+      return;
+    }
+  
+}
+
+void setInteractionTime(bool interaction, unsigned long *RTinteraction){
+  if(interaction == 1){
+    *RTinteraction = millis();
+    return;
+  }
+  return;
+}
+
+//find the amount of time that has passed since RTstart to current time. maximuim amount of time is 4294967295 milliseconds, ~50 days
+unsigned long RTdifference(unsigned long RTstart){
+    unsigned long RTcurrent = millis();
+    
+    if(RTstart <= RTcurrent){
+        return RTcurrent - RTstart;  
+    }
+
+    return (4294967295 - RTstart) + RTcurrent;
+  
+}
+
+//Turns on backlight after an interaction, and the backlight stays on for a set amount of time after the last interaction.
+void LCDbacklight(unsigned long RTinteraction){
+    //how low in millisec the backlight should remain lit
+    unsigned long RTblOnDelay = 15000;
+
+    if((RTdifference(RTinteraction) < RTblOnDelay) && !lcd.getBacklight()){
+      lcd.backlight();
+      return;
+    }
+    if((RTdifference(RTinteraction) > RTblOnDelay) && lcd.getBacklight()){
+       lcd.noBacklight();
+       return;
+    }
+
+}
+
+void LCDInitialize(){
+  
+  lcd.begin();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(6,0);
+  lcd.print("Byrd");
+  lcd.setCursor(3,1);
+  lcd.print("Protection");
+  delay(1000);
+}
+
+void LCDMessage(int message){
+  
+}
+
+
+
 
 void setupCheck(){
 
@@ -81,8 +186,10 @@ void userSetup(byte newSetup){
   byte input;
 
   if(newSetup == 1){
-    Serial.println("Please Enter a Four Digit Passcode");
-    
+    lcd.setCursor(0,0);
+    lcd.print("Please Enter a 4");
+    lcd.setCursor(0,1);
+    lcd.print("Digit Passcode");
   }
   
 }
